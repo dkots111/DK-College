236157fba1937a53b962cee4dbf5ba7d86e55093
dkots111 <dhvanankotecha111@gmail.com>
2019-08-08 15:45:26 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,25 +1,57 @@
-#include<iostream.h>
+#include<iostream>
 using namespace std;
 
-struct 2
+struct node
 {
     int data;
-    struct node *next;
+    node *next;
 };
-class Stack
-{
-    struct node *top;
+class stack
+{ struct node *top;
 public:
     void push(int x){
-        struct node *ptr;
+    struct node *ptr = new node();
         ptr->data = x;
-        ptr->next = NULL;
-        while (top!=NULL)
-        {
-            ptr->next=top;
+        ptr->next = top;
+        top=ptr;
+    }
+    void pop(){
+        struct node *ptr = top;
+        while(ptr!=NULL){
             top=ptr;
-            delete ptr;
+            top=top->next;
+            cout<<"Deleted element is"<<ptr->data;
+            delete(ptr);
         }
+    }
+    void display(){
+        struct node *ptr;
+            ptr = top;
+            cout<<"elements are : ";
+            while(ptr != NULL)
+            {
+                cout<<ptr->data<<"\t";
+                ptr = ptr->next;
+            }
+        }
+};
+int main(){
+int ch;
+    stack st;
+    while(1)
+    {
+        cout<<"\n1.push 2.pop  3.display 4.exit \n Enter your choice: ";
+        cin >> ch;
+        switch(ch)
+        {
+            case 1: cout <<"Enter the element ";
+                    cin >> ch;
+                    st.push(ch);
+                    break;
+            case 2: st.pop(); break;
+            case 3: st.display(); break;
+            case 4: exit(0);
         
+        }
     }
 }
